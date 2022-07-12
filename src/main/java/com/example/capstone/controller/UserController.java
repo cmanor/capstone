d9335598d7f0cf107bf0ce20dd3ee44fb5d1d800
cmanor <101116379+cmanor@users.noreply.github.com>
2022-07-12 16:31:37 -0400

    finished part 3 of crud tutorial, moving to security

--- src/main/java/com/example/capstone/controller/UserController.java
+++ src/main/java/com/example/capstone/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.example.capstone.controller;
 
+import com.example.capstone.model.User;
 import com.example.capstone.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -16,5 +19,17 @@ public class UserController {
     public String viewHomePage(Model model){
         model.addAttribute("listUsers", userService.getAllUsers());
         return "index";
+    }
+    @GetMapping("/showNewUserForm")
+    public String showNewUserForm(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "new_user";
+    }
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") User user) {
+        //save user to database
+        userService.saveUser(user);
+        return "redirect:/";
     }
 }
