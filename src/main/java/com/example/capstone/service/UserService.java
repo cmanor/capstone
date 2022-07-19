606f230cec45834e3451d22b8672ae78f96179a6
cmanor <101116379+cmanor@users.noreply.github.com>
2022-07-19 16:38:27 -0400

    onto tutorial part 7

--- src/main/java/com/example/capstone/service/UserService.java
+++ src/main/java/com/example/capstone/service/UserService.java
@@ -2,11 +2,12 @@ package com.example.capstone.service;
 
 import com.example.capstone.model.User;
 import com.example.capstone.web.dto.UserRegistrationDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     User save(UserRegistrationDto registrationDto);
     List<User> getAllUsers();
     void saveUser(User user);
--- src/main/java/com/example/capstone/service/UserServiceImpl.java
+++ src/main/java/com/example/capstone/service/UserServiceImpl.java
@@ -5,16 +5,26 @@ import com.example.capstone.model.User;
 import com.example.capstone.repository.UserRepository;
 import com.example.capstone.web.dto.UserRegistrationDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Autowired
+
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     public UserServiceImpl(UserRepository userRepository) {
         super();
@@ -34,8 +44,22 @@ public class UserServiceImpl implements UserService {
     public User save(UserRegistrationDto registrationDto) {
         User user = new User(registrationDto.getFirstName(),
                 registrationDto.getLastName(), registrationDto.getEmail(),
-                registrationDto.getPassword(), Arrays.asList(new Role("ROLE_USER")));
+                passwordEncoder.encode(registrationDto.getPassword()), Arrays.asList(new Role("ROLE_USER")));
 
         return userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
+       return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 }
