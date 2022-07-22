e1f97632e7fb5ad82a28d6dbae8ea3ac8933270a
cmanor <101116379+cmanor@users.noreply.github.com>
2022-07-22 12:19:56 -0400

    Adding CRUD for task and other things, doing tutorial, uploading to test

--- src/main/java/com/example/capstone/model/Task.java
+++ src/main/java/com/example/capstone/model/Task.java
@@ -1,4 +1,46 @@
 package com.example.capstone.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "task")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "task_name")
+    private String name;
+
+    @Column(name = "task_description")
+    private String description;
+
+
+    public Task(){}
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
